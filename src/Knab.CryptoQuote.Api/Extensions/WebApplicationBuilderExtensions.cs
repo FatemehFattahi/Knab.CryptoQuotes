5df3bbe446d28fdebee7f4fbdeb52b8b8b2c74e9
Fatemeh Fattahi <ftmfattahi@gmail.com>
2022-11-22 09:08:56 +0330

    add serilog

---
+++ src/Knab.CryptoQuote.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+
+namespace Knab.CryptoQuote.Api.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder UseSerilog(this WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((context, services, cfg) =>
+        {
+            cfg
+                .ReadFrom.Services(services)
+                .ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext();
+
+            const string consoleOutputTemplate =
+                "[{Timestamp:HH:mm:ss} {Level:u3}]:[{SourceContext}] {Message:lj} {NewLine}{Exception}";
+
+            cfg
+                .WriteTo
+                .Async(sinkCfg => sinkCfg.Console(outputTemplate: consoleOutputTemplate));
+        });
+
+        return builder;
+    }
+}
